6fd963b01aa7ee78b2eee0bd5b1e097253d0e2d6
nishu kumar <kumarnishu437@gmail.com>
2023-07-23 08:27:08 +0530

    updated

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
+    
   },
   preload: {
     plugins: [externalizeDepsPlugin()]
@@ -15,6 +16,12 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [react()]
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/api/v1': 'http://localhost:5000'
+        },
+        port:3000
+    }
   }
 })
